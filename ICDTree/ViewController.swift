ac5cd0ce0018c7d1cafc57d68e06d8985791a825
Fredy Davis <fredythekkekkara@gmail.com>
2018-09-20 12:15:08 +0530

    test

--- ICDTree/ViewController.swift
+++ ICDTree/ViewController.swift
@@ -73,9 +73,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return ROW_COUNT
     }
-//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 50
-//    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "icdCell")
 //        treeCell
